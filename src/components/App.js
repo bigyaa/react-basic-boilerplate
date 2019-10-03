044c44c2a09749e68f64fd02ec264f855cded3e8
bigya <bigyabajracharya@gmail.com>
2019-10-04 00:37:49 +0545

    Remove class and make it functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>The React Boilerplate works! Yay!</h1>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>
+      <h1>The React Boilerplate works! Yay!</h1>
+    </div>
+  )
 }
 
 export default App;
